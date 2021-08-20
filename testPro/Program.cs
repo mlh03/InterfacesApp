3ab8cab14fdd58a32c1be5f7fab2f83dc94d2be7
Mohamed Alsadi <mohamedalsadi@Mohameds-MacBook-Pro.local>
2021-08-20 15:20:57 -0500

    First commit

---
+++ testPro/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testPro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
